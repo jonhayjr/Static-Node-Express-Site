9c55a77a31bcce0bd38ad115d1b1663b61e356e0
jonhayjr <jonhayjr@gmail.com>
2021-05-01 19:50:44 -0700

    Added project logic and updated pugs templates

--- app.js
+++ app.js
@@ -6,14 +6,26 @@ const app = express();
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-app.get( '/', (req, res) => {
+app.get( '/', (req, res, next) => {
     res.render('index', {projects});
   });
 
-  app.get( '/about', ( req, res ) => {
+  app.get( '/about', (req, res, next) => {
     res.render('about');
   });
-  
+
+  app.get('/project/:id', (req, res, next) => {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === projectId );
+    
+    if (project) {
+
+      res.render('project', {project});
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!');
 });
