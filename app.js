bbd20cbd54e2c4a7496a5a3509cb5ee8de5e5822
jonhayjr <jonhayjr@gmail.com>
2021-05-02 15:41:10 -0700

    Made some tweaks to error loggin

--- app.js
+++ app.js
@@ -64,15 +64,16 @@ app.get('/project/error', (req, res, next) => {
 //Global Error Handler
 app.use((err, req, res, next) => {
     res.locals.error = err;
+
+    //Log error
+    console.log({err});
   
     //Throws Page Not Found error for 404 errors and generic error for all other errors.
     if (err.status === 404) {
-      console.log(`404 Error555: ${err.status} - ${err.message}`);
       res.status(err.status).render('page-not-found', {err});
     } else {
       err.message = err.message || 'Oops, something went wrong!';
       err.status = err.status || 500;
-      console.log(`An error has occurred: ${err.status} - ${err.message}`);
       res.status(err.status).render('error', {err});
     }
 });
