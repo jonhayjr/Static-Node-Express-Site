68d612cd471781c572c2ac5f2a2d6435946241fa
jonhayjr <jonhayjr@gmail.com>
2021-05-06 21:17:27 -0700

    Added error logging logic for 404 and generic errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const data = require('./data.json');
 const {projects} = data;
 const app = express();
+const logger = require('morgan');
 const port = process.env.PORT || 3000;
 
 
@@ -11,71 +12,69 @@ app.set('view engine', 'pug');
 //Static route setup
 app.use('/static', express.static('public'));
 
+//Logs request to page
+//app.use(logger('dev'));
+
 //Routes to Home page
 app.get( '/', (req, res, next) => {
-    res.render('index', {projects});
-  });
+  res.render('index', {projects});
+});
 
 //Routes to About page
 app.get( '/about', (req, res, next) => {
-    res.render('about');
-  });
+  res.render('about');
+});
 
- //Routes to Error page and throws custom error message
- app.get('/error', (req, res, next) => {
-    const err = new Error();
-    err.message = err.message || `Oops, something went wrong!`;
-    res.status(err.status = err.status || 500).render('error', {err});
-    console.log(`An error has occurred: ${err.status} - ${err.message}`);
+//Routes to Error page and throws custom error message
+app.get('/error', (req, res, next) => {
+const err = new Error();
+err.message = err.message || `Oops, something went wrong!`;
+res.status(err.status = err.status || 500).render('error', {err});
+console.log(`An error has occurred. (${err.status} - ${err.message})`);
 });
 
 //Routes to project/error page and throws custom error message
 app.get('/project/error', (req, res, next) => {
-  const err = new Error();
-  err.message = err.message || `Oops, something went wrong!`;
-  res.status(err.status = err.status || 500).render('error', {err});
-  console.log(`An error has occurred: ${err.status} - ${err.message}`);
+const err = new Error();
+err.message = err.message || `Oops, something went wrong!`;
+res.status(err.status = err.status || 500).render('error', {err});
+console.log(`An error has occurred.  (${err.status} - ${err.message})`);
 });
 
-
-
-  //Routes to individual project pages
-  app.get('/project/:id', (req, res, next) => {
-    const projectId = req.params.id;
-    const project = projects.find( ({ id }) => id === projectId );
-    //Checks if project data exists for specific id.  If not, a 404 error is thrown.
- 
-    if (project) {
-      res.render('project', {project});
-    } else {
-      const err = new Error();
+//Routes to individual project pages
+app.get('/project/:id', (req, res, next) => {
+  const projectId = req.params.id;
+  const project = projects.find( ({ id }) => id === projectId );
+  //Checks if project data exists for specific id.  If not, a 404 error is thrown.
+  if (project) {
+    res.render('project', {project});
+  } else {
+      const err = new Error( 'Page Not Found');
       err.status = 404;
-      err.message = 'Page not found';
-      //Passes error to global error handler
       next(err);
-    }
-  });
+  }
+});
 
-
-//404 Error Handler to catch undefined routes
+// catch 404 and forward to error handler
 app.use((req, res, next) => {
   const err = new Error();
   err.status = 404;
   err.message = 'Page Not Found';
   res.render('page-not-found', {err});
+  console.log(`404 error has occurred. (${err.status} - ${err.message})`);
 });
 
 //Global Error Handler
 app.use((err, req, res, next) => {
-  
-  //Log error to console
-    console.log(`${err.status} - ${err.message}`);
-    //Throws Page Not Found error for 404 errors and generic error for all other errors.
-    if (err.status === 404) {
-      res.status(err.status).render('page-not-found', {err});
-    } else {
-      err.message || 'Oops, something went wrong!'; 
-      res.status(err.status || 500).render('error', {err});
+
+  //Throws Page Not Found error for 404 errors and generic error for all other errors.
+  if (err.status === 404) {
+    res.status(err.status).render('page-not-found', {err});
+    console.log(`404 error has occurred. (${err.status} - ${err.message})`);
+  } else {
+    err.message || 'Oops, something went wrong!'; 
+    res.status(err.status || 500).render('error', {err});
+    console.log(`An error has occurred. (${err.status} - ${err.message})`);
     }
 });
 
